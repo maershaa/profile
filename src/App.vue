5ad0d0267035c6811a95b0f05dc05306d73dba7a
maershaa <valeria.work2022@gmail.com>
2025-06-19 14:49:43 +0300

    Add keyframes and styles for animations in the hero section

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,19 @@
       Hi 👋, My name is Valeriia Yefremova I build things for web
     </p>
 
+<div class="animations">
+ <img
+            src="./assets/images/hero/hero-star.png"
+            alt="star"
+            class="animation-star"
+          />
+
+          <div class="animation-circle-blue"> </div>
+          <div class="animation-circle-pink"> </div>
+</div>
+     
+
+
     <img src="./assets/images/my-photo.jpg" alt="my photo" class="main-photo" />
   </section>
 
